cb0c4811e603c134c014c8661b4030e19050e879
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-08 11:38:33 -0300

    add component header

--- App.tsx
+++ App.tsx
@@ -3,12 +3,19 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <>
       <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
         {fontsLoaded ? <Groups /> : <Loading />}
       </ThemeProvider>
     </>
