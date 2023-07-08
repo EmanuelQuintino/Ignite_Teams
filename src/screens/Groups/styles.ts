198fd94e7a73e322536d33b03040958456aeb15f
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-08 09:58:05 -0300

    add styled components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: black;
+  font-size: 32px;
+`;
