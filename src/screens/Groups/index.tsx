5f15441834cb3d63507a2c6967d6605d0bb6df05
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-09 20:38:12 -0300

    add GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,19 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header showBackButton={false} />
+
       <Highlight
         title='Turmas'
         subtitle='Jogue com sua turma'
       />
+
+      <GroupCard title='Galera do Ignite' onPress={() => { }} />
     </Container>
   );
 };
