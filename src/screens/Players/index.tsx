78bc2c866d235cea84d743943317ec7ce3e931ac
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-11 14:31:31 -0300

    add header list number of players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
 
 export function Players() {
+  const [teamActive, setTeamActive] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton={true} />
@@ -24,7 +29,22 @@ export function Players() {
         <ButtonIcon icon="add" type="ADD" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === teamActive}
+              onPress={() => setTeamActive(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 };
