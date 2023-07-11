8d0f3454b2e5c279483efddaec86a43230768db0
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-11 09:52:03 -0300

    refactor styled theme css

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,14 +1,16 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TextInput } from "react-native";
 
 export const Container = styled(TextInput)`
   flex: 1;
   min-height: 56px;
   max-height: 56px;
-  background: ${({ theme }) => theme.COLORS.GRAY_700};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
   border-radius: 8px;
   padding: 16px;
+  ${({ theme }) => css`
+    background: ${theme.COLORS.GRAY_700};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.WHITE};
+  `};
 `;
