ba1a6a26ad1e2a525128316bf9b4b2d9d2cd76d7
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-12 09:02:44 -0300

    add react native navigate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { Groups } from "@screens/Groups";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -17,9 +15,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {/* {fontsLoaded ? <NewGroup /> : <Loading />} */}
-        {/* {fontsLoaded ? <Groups /> : <Loading />} */}
-        {fontsLoaded ? <Players /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </ThemeProvider>
     </>
   );
--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports = function (api) {
         alias: {
           '@assets': './src/assets',
           '@components': './src/components',
-          '@routes': './src/routes',
           '@screens': './src/screens',
           '@storage': './src/storage',
           '@utils': './src/utils',
